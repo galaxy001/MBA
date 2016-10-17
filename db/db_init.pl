2d95ec176ee8688df01f8a8c479b6f698a1a8e38
Yuuki Galaxy <galaxy001@gmail.com>
2016-10-17 17:19:15 +0800

    modified:   db_init.pl

--- db/db_init.pl
+++ db/db_init.pl
@@ -9,6 +9,8 @@ my $infile = shift;
 my $dbfile = shift;
 $dbfile = 'tag.db' unless defined $dbfile;
 
+unlink $dbfile;
+
 my %attr = (
     RaiseError => 0,
     PrintError => 1,
@@ -18,26 +20,27 @@ my $dbh = DBI->connect('dbi:SQLite:dbname='.$dbfile,'','',\%attr) or die $DBI::e
 
 my $sql=q/
 CREATE TABLE IF NOT EXISTS RawData
-(  eid INTEGER,
-   colname TEXT,
-   thevalue TEXT
+(  eid INTEGER NOT NULL,
+   colname TEXT NOT NULL,
+   thevalue TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ColData
-( cid INTEGER,
-  colname TEXT,
-  cnt INTEGER
+( cid INTEGER NOT NULL,
+  colname TEXT NOT NULL,
+  aCnt INTEGER NOT NULL,
+  rCnt INTEGER NOT NULL DEFAULT(0)
 );
 CREATE TABLE IF NOT EXISTS ValueLists
-( vid INTEGER,
-  value TEXT,
-  cid INTEGER,
-  cnt INTEGER
+( vid INTEGER NOT NULL,
+  cid INTEGER NOT NULL,
+  thevalue TEXT NOT NULL,
+  cnt INTEGER NOT NULL
 );
 /;
 for (split /;/,$sql) {
 	next if /^\s*$/;
 	#s/{---}/$opt_s/g;
-#print "[$_]\n";
+	#print "[$_]\n";
 	$dbh->do($_) or die $dbh->errstr;
 }
 $dbh->commit;
@@ -55,15 +58,36 @@ sub openfile($) {
     return $infile;
 }
 
+my $sth = $dbh->prepare( "INSERT INTO RawData ( eid,colname,thevalue ) VALUES ( ?,?,? )" );
+
 my $in = openfile($infile);
 while (<$in>) {
 	chomp;
 	s/\r$//;
-	my @dat = split /,/,$_;
-	map {s/"//g} @dat;
-	#print join('|',@dat),"\n";
+	my @dat = split /,"/,$_;
+	map {s/"|^\s+|\s+$//g} @dat;
+	#print join('/',@dat),"\n";
+	if (@dat == 3) {
+		#print "$dat[0],$dat[1],$dat[2]\n";
+		if ( $dat[1] ~= /^Combined Samples \((\d+)\)$/) {
+			$dat[2] = "$1\t$dat[2]";
+			$dat[1] = "Combined Samples";
+		}
+		$sth->execute($dat[0],$dat[1],$dat[2]);
+	} else {
+		warn "[x]$_ => ",join('/',@dat),"\n";
+	}
 }
 
+$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData GROUP BY colname;" );
+my $sth2 = $dbh->prepare( "INSERT INTO ColData ( cid,colname,aCnt ) VALUES ( ?,?,? )" );
+
+
+$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData WHERE thevalue <> '' GROUP BY colname;" );
+$sth2 = $dbh->prepare( "INSERT INTO ColData ( cid,colname,rCnt ) VALUES ( ?,?,? )" );
+
+$dbh->commit;
+$dbh->disconnect;
 
 __END__
 ./db_init.pl jgi.all.info.fmt.csv.gz

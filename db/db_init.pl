e30997e73bad276ee8ad33ef5b601f89e046d817
Yuuki Galaxy <galaxy001@gmail.com>
2016-10-18 17:13:51 +0800

    modified:   db_init.pl

--- db/db_init.pl
+++ db/db_init.pl
@@ -9,7 +9,7 @@ my $infile = shift;
 my $dbfile = shift;
 $dbfile = 'tag.db' unless defined $dbfile;
 
-unlink $dbfile;
+truncate $dbfile,0;
 
 my %attr = (
     RaiseError => 0,
@@ -25,16 +25,16 @@ CREATE TABLE IF NOT EXISTS RawData
    thevalue TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ColData
-( cid INTEGER NOT NULL,
+( cid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   colname TEXT NOT NULL,
-  aCnt INTEGER NOT NULL,
-  rCnt INTEGER NOT NULL DEFAULT(0)
+  Cnt INTEGER NOT NULL
 );
 CREATE TABLE IF NOT EXISTS ValueLists
-( vid INTEGER NOT NULL,
+( vid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   cid INTEGER NOT NULL,
   thevalue TEXT NOT NULL,
-  cnt INTEGER NOT NULL
+  cnt INTEGER NOT NULL,
+  FOREIGN KEY (cid) REFERENCES "ColData" (cid)
 );
 /;
 for (split /;/,$sql) {
@@ -60,6 +60,9 @@ sub openfile($) {
 
 my $sth = $dbh->prepare( "INSERT INTO RawData ( eid,colname,thevalue ) VALUES ( ?,?,? )" );
 
+my (%EID2TaxID,$TaxIDcnt);
+
+print STDERR "[!]Loading: ...";
 my $in = openfile($infile);
 while (<$in>) {
 	chomp;
@@ -68,26 +71,46 @@ while (<$in>) {
 	map {s/"|^\s+|\s+$//g} @dat;
 	#print join('/',@dat),"\n";
 	if (@dat == 3) {
-		#print "$dat[0],$dat[1],$dat[2]\n";
-		if ( $dat[1] ~= /^Combined Samples \((\d+)\)$/) {
+		next if $dat[2] eq '';	# skip empay ones.
+		if ( $dat[1] =~ /^Combined Samples \((\d+)\)$/) {
 			$dat[2] = "$1\t$dat[2]";
 			$dat[1] = "Combined Samples";
+		} elsif ($dat[1] eq 'NCBI Taxon ID') {
+			$EID2TaxID{$dat[0]} = $dat[2];
 		}
 		$sth->execute($dat[0],$dat[1],$dat[2]);
 	} else {
 		warn "[x]$_ => ",join('/',@dat),"\n";
 	}
 }
+print STDERR "\b\b\bdone.\n[!]Building Index: ...";
+$sql=q/
+CREATE INDEX IF NOT EXISTS IDXeid ON "RawData" ("eid");
+CREATE INDEX IF NOT EXISTS IDXcolname ON "RawData" ("colname");
+/;
+for (split /;/,$sql) {
+	next if /^\s*$/;
+	$dbh->do($_) or die $dbh->errstr;
+}
+$dbh->commit;
+print STDERR "\b\b\bdone.\n";
 
-$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData GROUP BY colname;" );
-my $sth2 = $dbh->prepare( "INSERT INTO ColData ( cid,colname,aCnt ) VALUES ( ?,?,? )" );
-
-
-$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData WHERE thevalue <> '' GROUP BY colname;" );
-$sth2 = $dbh->prepare( "INSERT INTO ColData ( cid,colname,rCnt ) VALUES ( ?,?,? )" );
+$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData GROUP BY colname ORDER BY Cnt DESC;" );
+my $sth2 = $dbh->prepare( "INSERT INTO ColData ( colname,Cnt ) VALUES ( ?,? )" );
+$sth->execute();
+while (my $rv=$sth->fetchrow_arrayref) {
+	#ddx $rv;
+	if ($rv->[0] eq 'NCBI Taxon ID') {
+		$TaxIDcnt = $rv->[1];
+	}
+	$sth2->execute($rv->[0],$rv->[1]);
+}
+#$sth = $dbh->prepare( "SELECT colname, COUNT(colname) as Cnt from RawData WHERE thevalue <> '' GROUP BY colname;" );
+#$sth2 = $dbh->prepare( "INSERT INTO ColData ( cid,colname,rCnt ) VALUES ( ?,?,? )" );
 
 $dbh->commit;
 $dbh->disconnect;
 
+#ddx \%EID2TaxID;
 __END__
 ./db_init.pl jgi.all.info.fmt.csv.gz
